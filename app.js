9d38ef10867e4bbd8cdf957d4a0432f70e37d3b7
LuigiS-R <luigisr03@gmail.com>
2024-11-18 17:38:59 +0900

    General Update

--- app.js
+++ app.js
@@ -12,6 +12,51 @@ app.get('/', (req, res) => res.json({
     'message' : 'Hello World'
 }))
 
+app.get('/weather', (req, resp)=>{
+    axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+          lat: req.query.lat,
+          lon: req.query.lon,
+          appid: 'c1348e246938f5fa78a450f0e4f8a755'
+        }
+      })
+      .then(response => {
+        resp.json({
+            'location': response.data.name,
+            'temperature': response.data.main.temp,
+            'humidity': response.data.main.humidity
+            
+        })
+        console.log(response)
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+})
+
+app.get('/pollution', (req, resp)=>{
+    axios.get('http://api.openweathermap.org/data/2.5/air_pollution', {
+        params: {
+          lat: req.query.lat,
+          lon: req.query.lon,
+          appid: 'c1348e246938f5fa78a450f0e4f8a755'
+        }
+      })
+      .then(response => {
+        resp.json({
+            'pm2_5': response.data.list[0].components.pm2_5,
+            'pm10': response.data.list[0].components.pm10,
+            'nh3': response.data.list[0].components.nh3,
+            'so2': response.data.list[0].components.so2
+            
+        })
+        console.log(response)
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+})
+
 app.listen(3000, ()=>{
     console.log('Listening on port 3000')
 })
--- main.js
+++ main.js
@@ -1,8 +1,56 @@
 function onLoad(){
-    const requestUrl = `http://localhost:3000/weather`;
-    fetch(requestUrl)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.name)
-        })
+
+    const currentDate = new Date();
+    const time = currentDate.getHours();
+    console.log(time);
+
+    var coordinates = {latitude:0, coordinates:0}
+
+    var getLocation = new Promise((resolve, reject)=>{
+        if (!navigator.geolocation){
+            console.log('Error, Geolocation not available')
+        }
+    
+        function success(pos){
+            coordinates.latitude = pos.coords.latitude
+            coordinates.longitude = pos.coords.longitude
+
+            const requestUrl = `http://localhost:3000/weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}`;
+            fetch(requestUrl)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    document.getElementById("locationName").textContent = (data.location).toUpperCase();
+                    if (time >= 6 && time < 18){
+                        document.getElementById('timeImage').src = './Images/sun.png'
+                    }
+                    else{
+                        document.getElementById('timeImage').src = './Images/moon.svg'
+                    }
+                    document.getElementById("dynamicTemperature").innerHTML ="<b>" + parseInt(data.temperature - 273.15, 10)+ "&#176;C</b>";
+                    document.getElementById("dynamicHumidity").innerHTML ="<b>" + parseInt(data.humidity)+ "%</b>";
+                })
+
+            const requestUrl2 = `http://localhost:3000/pollution?lat=${coordinates.latitude}&lon=${coordinates.longitude}`;
+            fetch(requestUrl2)
+                .then(response => response.json())
+                .then(data => {
+                    document.getElementById("dynamicPm25").innerHTML = "<b>"+data.pm2_5+" &#956;g/m&sup3;</b>";
+                    document.getElementById("dynamicPm10").innerHTML = "<b>"+data.pm10+" &#956;g/m&sup3;</b>";
+                    document.getElementById("dynamicNh3").innerHTML = "<b>"+data.nh3+" ppb</b>";
+                    document.getElementById("dynamicSo2").innerHTML = "<b>"+data.so2+" ppb</b>";
+                })
+        }
+    
+        function error(){
+            reject('Error while retrieving the location')
+        }
+    
+        navigator.geolocation.getCurrentPosition(success, error)
+    })
+}
+
+function getForecast(){
+    console.log("getForecast");
+    document.getElementById("forecastContainer").style.height = "100px";
 }
